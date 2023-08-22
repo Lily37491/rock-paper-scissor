b63151d0a297b52b674ba5d191976b87a5bed716
Payal Kumavat <payalkumavat02@gmail.com>
2023-08-22 16:28:35 +0530

    Generate prompt(), lets the user type their choice

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
+const playerChoice = prompt("Rock, Paper or Scissors", "Rock");
+
 function getComputerChoice() {
     const choice = ["Rock", "Paper", "Scissor"]
     const computerChoice = choice[Math.floor(Math.random() * 3)];
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+
 
 
