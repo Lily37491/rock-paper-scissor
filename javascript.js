98eca5f00a734d4f267a3d48402a5b845490b061
Payal Kumavat <payalkumavat02@gmail.com>
2023-09-11 16:13:17 +0530

    Play the game for exactly 5 rounds

--- javascript.js
+++ javascript.js
@@ -5,20 +5,56 @@ function getComputerChoice() {
 }
 
 const userBtn = document.querySelectorAll("#user-btn");
+let message = document.querySelector("#message");
+const playerScore = document.querySelector(".player-score");
+const compScore = document.querySelector(".comp-score");
+const playBtn = document.querySelector(".play-btn");
 
+userScore = 0;
+computerScore = 0;
+Round = 0;
+maxRound = 5;
 userBtn.forEach(button => {
     button.addEventListener('click', () =>{
         const userChoice = button.getAttribute('data-choice');
         const compChoice = getComputerChoice();
 
+        if(Round >= maxRound){
+            return;
+        } 
         if(userChoice === compChoice){
-            console.log("It's a tie");
-        } else if(userChoice === "rock" && compChoice === "scissor" ||
-                    userChoice === "paper" && compChoice === "rock" ||
-                    userChoice === "scissor" && compChoice === "paper"){
-            console.log("you score a point");
-        } else {
-            console.log("comp scores a point");
+            message.textContent = "It's a Tie";
+        } else if(userChoice === "rock" && compChoice === "scissor"){
+            message.textContent = "You score a point";
+            userScore++;
+            playerScore.textContent = `Player: ${userScore}`;
+        } else if(userChoice === "paper" && compChoice === "rock"){
+            message.textContent = "You score a point";
+            userScore++;
+            playerScore.textContent = `Player: ${userScore}`;
+        } else if(userChoice === "scissor" && compChoice === "paper"){
+            message.textContent = "You score a point";
+            userScore++;
+            playerScore.textContent = `Player: ${userScore}`;
+        }else if(compChoice === "rock" && userChoice === "scissor"){
+            message.textContent = "Computer scores a point";
+            computerScore++;
+            compScore.textContent = `Computer: ${computerScore}`;
+        }else if(compChoice === "paper" && userChoice === "rock"){
+            message.textContent = "Computer scores a point";
+            computerScore++;
+            compScore.textContent = `Computer: ${computerScore}`;
+        }else if(compChoice === "scissor" && userChoice === "paper"){
+            message.textContent =  "Computer scores a point";
+            computerScore++;
+            compScore.textContent = `Computer: ${computerScore}`;
         }
+
+        Round++;
     })
+
 })
+
+
+
+
