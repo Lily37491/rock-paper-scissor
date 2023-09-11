5b668bbc1611d6a19ad78dd466d0b66a6468d613
Payal Kumavat <payalkumavat02@gmail.com>
2023-09-11 17:48:24 +0530

    Delete everything except random Computer Choice
    The code got messy and I coudn't get the winner in the end.

--- javascript.js
+++ javascript.js
@@ -4,56 +4,6 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-const userBtn = document.querySelectorAll("#user-btn");
-let message = document.querySelector("#message");
-const playerScore = document.querySelector(".player-score");
-const compScore = document.querySelector(".comp-score");
-const playBtn = document.querySelector(".play-btn");
-
-userScore = 0;
-computerScore = 0;
-Round = 0;
-maxRound = 5;
-userBtn.forEach(button => {
-    button.addEventListener('click', () =>{
-        const userChoice = button.getAttribute('data-choice');
-        const compChoice = getComputerChoice();
-
-        if(Round >= maxRound){
-            return;
-        } 
-        if(userChoice === compChoice){
-            message.textContent = "It's a Tie";
-        } else if(userChoice === "rock" && compChoice === "scissor"){
-            message.textContent = "You score a point";
-            userScore++;
-            playerScore.textContent = `Player: ${userScore}`;
-        } else if(userChoice === "paper" && compChoice === "rock"){
-            message.textContent = "You score a point";
-            userScore++;
-            playerScore.textContent = `Player: ${userScore}`;
-        } else if(userChoice === "scissor" && compChoice === "paper"){
-            message.textContent = "You score a point";
-            userScore++;
-            playerScore.textContent = `Player: ${userScore}`;
-        }else if(compChoice === "rock" && userChoice === "scissor"){
-            message.textContent = "Computer scores a point";
-            computerScore++;
-            compScore.textContent = `Computer: ${computerScore}`;
-        }else if(compChoice === "paper" && userChoice === "rock"){
-            message.textContent = "Computer scores a point";
-            computerScore++;
-            compScore.textContent = `Computer: ${computerScore}`;
-        }else if(compChoice === "scissor" && userChoice === "paper"){
-            message.textContent =  "Computer scores a point";
-            computerScore++;
-            compScore.textContent = `Computer: ${computerScore}`;
-        }
-
-        Round++;
-    })
-
-})
 
 
 
