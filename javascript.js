47bb2647f1efff2dc34b30abaa68af6bc6df356a
Payal Kumavat <payalkumavat02@gmail.com>
2023-09-11 09:36:08 +0530

    Fetch user choice by data-choice attribute

--- javascript.js
+++ javascript.js
@@ -1,6 +1,14 @@
 function getComputerChoice() {
-    const choice = ["Rock", "Paper", "Scissor"]
+    const choice = ["rock", "paper", "scissor"]
     let computerChoice = choice[Math.floor(Math.random() * 3)];
     return computerChoice;
 }
 
+const userBtn = document.querySelectorAll("#user-btn");
+
+userBtn.forEach(button => {
+    button.addEventListener('click', () =>{
+        const userChoice = button.getAttribute('data-choice');
+        console.log(userChoice);
+    })
+})
