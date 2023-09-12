649315dd7e772640bf27f8706f1f67795ba58abf
Payal Kumavat <payalkumavat02@gmail.com>
2023-09-12 10:42:49 +0530

    feat: Added game functionality with timer
    Timer has some issue when the play button is pressed in middle of game.

--- javascript.js
+++ javascript.js
@@ -4,7 +4,83 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+const userBtn = document.querySelectorAll("#user-btn");
+const playerScore = document.querySelector(".player-score");
+const compScore = document.querySelector(".comp-score");
+const message = document.querySelector("#message");
+let userScore = 0;
+let computerScore = 0;
+let Round = 0;
+const maxRound = 5;
+const endMessage = document.querySelector("#end-message");
+const playBtn = document.querySelector("#play-btn");
+const timeLeft = document.querySelector("#timer");
 
+playBtn.addEventListener('click', playGame);
+
+function startTimer(){
+    let seconds = 30;
+
+    timer = setInterval(() => {
+        if(seconds >= 0){
+            timeLeft.textContent = `Time left: ${seconds}`;
+            seconds--;
+        } else if(seconds <= 0){
+            clearInterval(timer);
+            
+        }
+    },1000);
+
+}
+
+function playGame(){
+    startGame()
+    startTimer();
+}
+function startGame(){
+userBtn.forEach(button => {
+    button.addEventListener('click', () => {
+
+        if (Round >= maxRound){
+            gameEnd();
+            return;
+        }
+        const userChoice = button.getAttribute("data-choice");
+        const computerChoice = getComputerChoice();
+
+        if(userChoice === computerChoice){
+            message.textContent = "Tie"
+        } else if(userChoice === "rock" && computerChoice === "scissor"||
+                userChoice === "paper" && computerChoice === "rock"||
+                userChoice === "scissor" && computerChoice === "paper"){
+            message.textContent = "You get a point";
+            userScore++;
+            playerScore.textContent = `Player: ${userScore}`;
+        }else {
+            message.textContent = "Computer scores a point";
+            computerScore++;
+            compScore.textContent = `Computer: ${computerScore}`;
+        }
+
+        Round++;
+
+
+    })
+})
+}
+
+function gameEnd(){
+    if(userScore > computerScore){
+        message.textContent = "";
+        endMessage.textContent = "YOU WIN!"
+    } else if(computerScore > userScore){
+        message.textContent = ""
+        endMessage.textContent = "YOU LOSE"
+    } else {
+        message.textContent = ""
+        endMessage.textContent = "IT'S A TIE. PLAY AGAIN TO WIN"
+    }
+}
 
 
 
